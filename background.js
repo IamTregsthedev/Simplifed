a9ea9a259599c39b83db0c7587c3abe91aeedc31
Arav Narula <aravnarula18@gmail.com>
2019-02-16 23:36:43 -0500

    remove duplicate

--- background.js
+++
@@ -1,7 +0,0 @@
-alert(
-  "hello, Thanks for using SimplePick. This was Developed by Kerrtravers.com, Aravnarula.com, And https://github.com/Smrman"
-);
-
-alert(
-  "Want to opt out of thesse alerts? Email media.trouge@gmail.com, for a Unpacked copy :"
-);
