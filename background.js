516baac39aa869e6a70a66484a3d85de3896694e
Arav Narula <proteximportsexports@gmail.com>
2019-02-16 23:22:23 -0500

    Add opt out

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 alert(
   "hello, Thanks for using SimplePick. This was Developed by Kerrtravers.com, Aravnarula.com, And https://github.com/Smrman"
 );
+
+alert("Want to opt out of thesse alerts? Email media.trouge@gmail.com, for a Unpacked copy :");
